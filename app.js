e43178f18594aa2f1c5be2ce315d453618c12d2d
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-07-24 21:28:23 +0100

    species display expanded to include labels

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const arrayFromChecklist = checklist => {
     return array.filter(child => child.checked).map(child => child.value);
 };
 
-const getValuesFromData = data => {
+const parseData = data => {
     // => object holding input values from event target
     return {
         commonName: data.commonName.value,
@@ -17,38 +17,86 @@ const getValuesFromData = data => {
     };
 };
 
-const logValues = values => {
+const logValues = data => {
     console.log("=".repeat(40));
     console.log("New Species Submitted with the following data:");
     console.log("-".repeat(40));
-    console.log('commonName :>> ', values.commonName);
-    console.log('scientificName :>> ', values.scientificName);
-    console.log('habitat :>> ', values.habitat);
-    console.log('range :>> ', values.range);
-    console.log('population :>> ', values.population);
-    console.log('conservationStatus :>> ', values.conservationStatus);
+    console.log('commonName :>> ', data.commonName);
+    console.log('scientificName :>> ', data.scientificName);
+    console.log('habitat :>> ', data.habitat);
+    console.log('range :>> ', data.range);
+    console.log('population :>> ', data.population);
+    console.log('conservationStatus :>> ', data.conservationStatus);
     console.log("=".repeat(40));
 };
 
-const populateListItem = (listItem, values) => {
-    listItem.textContent = "test";
+const getSpeciesTitle = data => {
+    return data.commonName ? data.commonName : data.scientificName;
+};
+
+const addListItem = (parent, text) => {
+    const newListItem = document.createElement("li");
+    newListItem.textContent = text;
+    parent.appendChild(newListItem);
+};
+
+const conservationStatusDisplay = status => {
+    const displayList = {
+        EX: "EX (Extinct",
+        EW: "EW (Extinct in the wild",
+        CR: "CR (Critically endangered)",
+        EN: "EN (Endangered)",
+        VU: "VU (Vulnerable)",
+        NT: "NT (Near threatened)",
+        LC: "LC (Least concern)",
+        DD: "DD (Data deficient)",
+        NE: "NE (Not evaluated)"
+    };
+    return `Conservation Status: ${displayList[status]}`
+};
+
+const rangeDisplay = rangeList => {
+    const range =  rangeList.join(", ")
+    return `Range: ${range}`
+};
+
+const populateSpeciesListItem = (listItem, data) => {
+    const title = document.createElement("h2");
+    title.classList.add("species-title");
+    title.textContent = getSpeciesTitle(data);
+    listItem.appendChild(title);
+
+    if (data.commonName) {
+        const subtitle = document.createElement("h3");
+        subtitle.classList.add("species-subtitle");
+        subtitle.textContent = data.scientificName;
+        listItem.appendChild(subtitle);
+    }
+
+    const body = document.createElement("ul");
+    body.classList.add("species-body");
+    addListItem(body, conservationStatusDisplay(data.conservationStatus));
+    addListItem(body, `Estimated Population in the Wild: ${data.population}`);
+    addListItem(body, rangeDisplay(data.range));
+    addListItem(body, `Habitat: ${data.habitat}`);
+    listItem.appendChild(body);
 };
 
 
 
 
 // EVENT HANDLERS
-const handleNewAnimal = function () {
+const handleNewSpecies = function () {
     event.preventDefault()
 
-    const values = getValuesFromData(this);
-    logValues(values);
+    const data = parseData(this);
+    logValues(data);
     
-    const newListItem = document.createElement("li");
-    populateListItem(newListItem, values)
+    const newSpeciesListItem = document.createElement("li");
+    populateSpeciesListItem(newSpeciesListItem, data)
     
-    const list = document.querySelector("#species-list");
-    list.appendChild(newListItem);
+    const speciesList = document.querySelector("#species-list");
+    speciesList.appendChild(newSpeciesListItem);
 
     this.reset();
 };
@@ -58,6 +106,6 @@ const handleNewAnimal = function () {
 document.addEventListener("DOMContentLoaded", () => {
     console.log("js loaded");
 
-    const newAnimalForm = document.querySelector("#new-animal-form");
-    newAnimalForm.addEventListener("submit", handleNewAnimal);
+    const newSpeciesForm = document.querySelector("#new-species-form");
+    newSpeciesForm.addEventListener("submit", handleNewSpecies);
 });
