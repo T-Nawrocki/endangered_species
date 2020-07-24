dcb363e6a8dd65658f5533363cc5cd2bffbe2229
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-07-24 20:27:11 +0100

    new list item created on submit event, and placeholder data population added; some refactoring to getting and logging form values

--- app.js
+++ app.js
@@ -3,28 +3,52 @@ const arrayFromChecklist = checklist => {
     // => array of values from checked elements of NodeList
     array = Array.from(checklist)
     return array.filter(child => child.checked).map(child => child.value);
-}
+};
+
+const getValuesFromData = data => {
+    // => object holding input values from event target
+    return {
+        commonName: data.commonName.value,
+        scientificName: data.scientificName.value,
+        habitat: data.habitat.value,
+        range: arrayFromChecklist(data.range),
+        population: data.population.value,
+        conservationStatus: data.conservationStatus.value  
+    };
+};
+
+const logValues = values => {
+    console.log("=".repeat(40));
+    console.log("New Species Submitted with the following data:");
+    console.log("-".repeat(40));
+    console.log('commonName :>> ', values.commonName);
+    console.log('scientificName :>> ', values.scientificName);
+    console.log('habitat :>> ', values.habitat);
+    console.log('range :>> ', values.range);
+    console.log('population :>> ', values.population);
+    console.log('conservationStatus :>> ', values.conservationStatus);
+    console.log("=".repeat(40));
+};
+
+const populateListItem = (listItem, values) => {
+    listItem.textContent = "test";
+};
+
+
 
 
 // EVENT HANDLERS
 const handleNewAnimal = function () {
     event.preventDefault()
 
-    const commonName = this.commonName.value;
-    const scientificName = this.scientificName.value;
-    const habitat = this.habitat.value;
-    const range = arrayFromChecklist(this.range);
-    const population = this.population.value;
-    const conservationStatus = this.conservationStatus.value;
-
-    console.log('event :>> ', event);
-    console.log('new animal :>> ', this);
-    console.log('commonName :>> ', commonName);
-    console.log('scientificName :>> ', scientificName);
-    console.log('habitat :>> ', habitat);
-    console.log('range :>> ', range);
-    console.log('population :>> ', population);
-    console.log('conservationStatus :>> ', conservationStatus);
+    const values = getValuesFromData(this);
+    logValues(values);
+    
+    const newListItem = document.createElement("li");
+    populateListItem(newListItem, values)
+    
+    const list = document.querySelector("#species-list");
+    list.appendChild(newListItem);
 
     this.reset();
 };
